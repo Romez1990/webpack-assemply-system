7cfe4771179740b09f27d5c1dc958ae18e0e0c90
Romez1990 <Romez19900@gmail.com>
2019-07-29 11:06:18 +0300

    Create devtool module

--- webpack/dev.js
+++ webpack/dev.js
@@ -1,5 +1,10 @@
 const merge = require('webpack-merge');
 const common = require('./common');
 const mode = require('./modules/mode');
+const devtool = require('./modules/devtool');
 
-module.exports = merge(common(), mode('development'));
+module.exports = merge(
+  common(),
+  mode('development'),
+  devtool('inline-cheap-module-source-map'),
+);
