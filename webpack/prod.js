44e1ae8e3d0fa91e56ba689e08cffccc15be9b7e
Romez1990 <Romez19900@gmail.com>
2019-07-31 21:36:25 +0300

    Add js uglifying

--- webpack/prod.js
+++ webpack/prod.js
@@ -1,5 +1,6 @@
 const merge = require('webpack-merge');
 const common = require('./common');
 const mode = require('./modules/mode');
+const minifyJS = require('./modules/minifyJS');
 
-module.exports = merge(common(), mode('production'));
+module.exports = merge(common(), mode('production'), minifyJS());
